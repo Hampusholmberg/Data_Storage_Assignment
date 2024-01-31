71711389c6348bc4901cb519155f6ca7b0f17cc1
Hampusholmberg <117671157+Hampusholmberg@users.noreply.github.com>
2024-01-31 11:52:54 +0100

    .

--- Infrastructure/Contexts/ProductCatalogContext.cs
+++ Infrastructure/Contexts/ProductCatalogContext.cs
@@ -28,61 +28,55 @@ public partial class ProductCatalogContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Education\\04._Datalagring\\Data_Storage_Assignment\\Infrastructure\\Data\\ProductCatalog\\ProductCatalog.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
-
         optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddFilter((category, level) => level >= LogLevel.Warning)));
     }
-
-
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Brand>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Brands__3214EC07F681D5CC");
+            entity.HasKey(e => e.Id).HasName("PK__Brands__3214EC071E8F3256");
 
             entity.Property(e => e.BrandName).HasMaxLength(100);
         });
 
         modelBuilder.Entity<Category>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC07B420FFD5");
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC077A623193");
 
             entity.Property(e => e.CategoryName).HasMaxLength(100);
         });
 
         modelBuilder.Entity<Product>(entity =>
         {
-            entity.HasKey(e => e.ArticleNumber).HasName("PK__Products__3C99114327E84F9E");
+            entity.HasKey(e => e.ArticleNumber).HasName("PK__Products__3C99114398020F9B");
 
             entity.Property(e => e.Price).HasColumnType("money");
             entity.Property(e => e.Title).HasMaxLength(100);
 
             entity.HasOne(d => d.Brand).WithMany(p => p.Products)
                 .HasForeignKey(d => d.BrandId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Products__BrandI__52593CB8");
+                .HasConstraintName("FK__Products__BrandI__08B54D69");
 
             entity.HasOne(d => d.Category).WithMany(p => p.Products)
                 .HasForeignKey(d => d.CategoryId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Products__Catego__5070F446");
+                .HasConstraintName("FK__Products__Catego__06CD04F7");
 
             entity.HasOne(d => d.SubCategory).WithMany(p => p.Products)
                 .HasForeignKey(d => d.SubCategoryId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Products__SubCat__5165187F");
+                .HasConstraintName("FK__Products__SubCat__07C12930");
         });
 
         modelBuilder.Entity<SubCategory>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__SubCateg__3214EC07ED05F624");
+            entity.HasKey(e => e.Id).HasName("PK__SubCateg__3214EC07ED52A768");
 
             entity.Property(e => e.SubCategoryName).HasMaxLength(100);
 
             entity.HasOne(d => d.Category).WithMany(p => p.SubCategories)
                 .HasForeignKey(d => d.CategoryId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__SubCatego__Categ__4BAC3F29");
+                .HasConstraintName("FK__SubCatego__Categ__02084FDA");
         });
 
         OnModelCreatingPartial(modelBuilder);
